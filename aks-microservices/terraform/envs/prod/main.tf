52b34160265e8e585495bf97af7dc9d664dd0629
anuj.toppo <anuj.toppo@outlook.com>
2025-11-15 21:41:32 +0530

    made changes to checkov scan workflow and changed module location on root tf file

--- aks-microservices/terraform/envs/prod/main.tf
+++ aks-microservices/terraform/envs/prod/main.tf
@@ -1,6 +1,6 @@
 
 module "rg" {
-  source   = "git::https://github.com/anujtoppoMS/aks-microservices.git//aks-microservices/terraform/modules/resource_group?ref=74d6c276a63c91ee7dc6438a29a8cfb232c46816"
+  source   = "./aks-microservices/terraform/modules/resource_group"
   for_each = var.resource_groups
 
   rg_name  = each.key
@@ -10,7 +10,7 @@ module "rg" {
 
 #networking hub
 module "hub" {
-  source              = "git::https://github.com/anujtoppoMS/aks-microservices.git//aks-microservices/terraform/modules/networking/hub?ref=74d6c276a63c91ee7dc6438a29a8cfb232c46816"
+  source              = "./aks-microservices/terraform/modules/networking/hub"
   resource_group_name = module.rg["rg_hub"].name
   location            = module.rg["rg_hub"].location
   spoke_vnet_id       = module.spoke.spoke_vnet_id
@@ -18,7 +18,7 @@ module "hub" {
 
 #networking aks spoke
 module "spoke" {
-  source              = "git::https://github.com/anujtoppoMS/aks-microservices.git//aks-microservices/terraform/modules/networking/spoke?ref=74d6c276a63c91ee7dc6438a29a8cfb232c46816"
+  source              = "./aks-microservices/terraform/modules/networking/spoke"
   resource_group_name = module.rg["rg_aks_spoke"].name
   location            = module.rg["rg_aks_spoke"].location
   hub_vnet_id         = module.hub.hub_vnet_id
@@ -26,7 +26,7 @@ module "spoke" {
 
 # ACR attached to the AKS
 module "acr" {
-  source              = "git::https://github.com/anujtoppoMS/aks-microservices.git//aks-microservices/terraform/modules/acr?ref=74d6c276a63c91ee7dc6438a29a8cfb232c46816"
+  source              = "./aks-microservices/terraform/modules/acr"
   resource_group_name = module.rg["rg_acr"].name
   location            = module.rg["rg_acr"].location
   acr_name            = "peerisland-asses-acr"
@@ -36,7 +36,7 @@ module "acr" {
 data "azurerm_client_config" "current" {}
 
 module "keyvault" {
-  source              = "git::https://github.com/anujtoppoMS/aks-microservices.git//aks-microservices/terraform/modules/keyvault?ref=74d6c276a63c91ee7dc6438a29a8cfb232c46816"
+  source              = "./aks-microservices/terraform/modules/keyvault"
   resource_group_name = module.rg["rg_keyvault"].name
   location            = module.rg["rg_keyvault"].location
   kv_name             = "peerisland-asses-kv"
@@ -48,7 +48,7 @@ module "keyvault" {
 
 #aks module
 module "aks" {
-  source              = "git::https://github.com/anujtoppoMS/aks-microservices.git//aks-microservices/terraform/modules/aks?ref=74d6c276a63c91ee7dc6438a29a8cfb232c46816"
+  source              = "./aks-microservices/terraform/modules/aks"
   resource_group_name = module.rg["rg_aks_microservices"].name
   location            = module.rg["rg_aks_microservices"].location
   subnet_ids = {
