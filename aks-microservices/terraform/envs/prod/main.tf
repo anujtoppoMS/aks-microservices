05f479aef4774397e50a18af91238ed19bc3896d
anuj.toppo <anuj.toppo@outlook.com>
2025-11-17 11:42:34 +0530

    modified kube provider cred pass

--- aks-microservices/terraform/envs/prod/main.tf
+++ aks-microservices/terraform/envs/prod/main.tf
@@ -66,10 +66,10 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  host                   = module.aks.kube_config_raw.0.host
-  client_certificate     = base64decode(module.aks.kube_config_raw.0.client_certificate)
-  client_key             = base64decode(module.aks.kube_config_raw.0.client_key)
-  cluster_ca_certificate = base64decode(module.aks.kube_config_raw.0.cluster_ca_certificate)
+  host                   = module.aks.kube_config_raw.host
+  client_certificate     = base64decode(module.aks.kube_config_raw.client_certificate)
+  client_key             = base64decode(module.aks.kube_config_raw.client_key)
+  cluster_ca_certificate = base64decode(module.aks.kube_config_raw.cluster_ca_certificate)
 }
 
 module "secretprovider" {
