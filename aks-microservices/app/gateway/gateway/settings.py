82c88318e776f0cde6394896b730c1015cce3073
anuj.toppo <anuj.toppo@outlook.com>
2025-11-20 01:43:38 +0530

    simplified the app secret supply

--- aks-microservices/app/gateway/gateway/settings.py
+++ aks-microservices/app/gateway/gateway/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.open("/mnt/secrets-store/django-secret-key").read().strip()
+# SECRET_KEY = os.open("/mnt/secrets-store/django-secret-key").read().strip()
+SECRET_KEY = "djnago-ecret"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.open("/mnt/secrets-store/allowed-hosts").read().strip()]
+# ALLOWED_HOSTS = [os.open("/mnt/secrets-store/allowed-hosts").read().strip()]
+
+ALLOWED_HOSTS = ["172.212.25.96"]
 
 
 # Application definition
