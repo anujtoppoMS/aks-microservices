a2bec2d7eaa05f31265d38e65832c400a102b862
anuj.toppo <anuj.toppo@outlook.com>
2025-11-20 08:34:05 +0530

    modified the app routing

--- aks-microservices/app/gateway/users/views.py
+++ aks-microservices/app/gateway/users/views.py
@@ -1,5 +1,6 @@
 import requests
 from django.shortcuts import render
+from django.http import HttpResponse
 
 USER_SERVICE = "http://user-service:8001/users"
 
@@ -7,3 +8,6 @@ def list_users(request):
     resp = requests.get(USER_SERVICE)
     users = resp.json()
     return render(request, "users/list.html", {"users": users})
+
+def index(request):
+    return HttpResponse("<h1>Welcome to My Django App</h1><p>This is the index page.</p>")
