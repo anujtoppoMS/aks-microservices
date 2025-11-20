6dbb6db7550667cc9b5d3383e8fb30f653aacb20
anuj.toppo <anuj.toppo@outlook.com>
2025-11-20 09:16:49 +0530

    updated urls

--- aks-microservices/app/gateway/gateway/urls.py
+++ aks-microservices/app/gateway/gateway/urls.py
@@ -19,17 +19,13 @@ from django.urls import path, include
 from users.views import index
 from django.http import HttpResponse
 
-
-
 def health_check(request):
     return HttpResponse("OK")
-
-
 urlpatterns = [
-    path("", index, name="index"),
-    path('admin/', admin.site.urls),
-    path("users/", include("users.urls")),
-    path("orders/", include("orders.urls")),
-    path('metrics/', include('django_prometheus.urls')),
-    path("healthz/", health_check),
+    path("/", index, name="index"),
+    path('/admin', admin.site.urls),
+    path("/users", include("users.urls")),
+    path("/orders", include("orders.urls")),
+    path("/metrics", include('django_prometheus.urls')),
+    path("/healthz", health_check),
 ]
