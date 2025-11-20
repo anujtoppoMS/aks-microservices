7c63601b5f02ba070739e73df787e1473cd944c0
anuj.toppo <anuj.toppo@outlook.com>
2025-11-20 07:56:18 +0530

    modified settings

--- aks-microservices/app/gateway/gateway/settings.py
+++ aks-microservices/app/gateway/gateway/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django_prometheus.middleware.PrometheusBeforeMiddleware',
+    # 'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
