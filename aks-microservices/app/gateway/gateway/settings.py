a8079b0c340d1c96c55b724b84b518ac543972b9
anuj.toppo <anuj.toppo@outlook.com>
2025-11-20 11:20:28 +0530

    updated settings

--- aks-microservices/app/gateway/gateway/settings.py
+++ aks-microservices/app/gateway/gateway/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'gateway.urls'
